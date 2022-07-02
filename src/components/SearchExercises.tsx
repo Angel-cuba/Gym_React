09fd3820c1f0bd9d53789f4a0aa598b8ec71552e
Mac_Developer <araozangel842@gmail.com>
2022-07-02 15:45:57 +0300

    Search component

--- src/components/SearchExercises.tsx
+++ src/components/SearchExercises.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
-  )
-}
+    <Stack alignItems="center" mt="38px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: '50px', xs: '30px' } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome exercises you <br />
+        Can do with your body
+      </Typography>
+    </Stack>
+  );
+};
 
-export default SearchExercises
+export default SearchExercises;
