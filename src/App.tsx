8e744ede6c2f853d09564ac81e91a77ec345bc01
Mac_Developer <araozangel842@gmail.com>
2023-04-05 11:44:47 +0300

    Refactoring router

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import ExerciseDetails from './pages/ExerciseDetails';
 import Navbar from './components/Navbar/Navbar';
-import Home from './pages/Home';
 import Footer from './components/Footer/Footer';
 import './App.css';
 
+const Home = React.lazy(() => import('./pages/Home'));
+const ExerciseDetails = React.lazy(() => import('./pages/ExerciseDetails'));
+
 function App() {
-  return <Box width="400px" sx={{width: {xl: '1488px'}}} m="auto">
-    <Navbar/>
-    <Routes>
-      <Route path="" element={<Home/>} />
-      <Route path="/" element={<Home/>} />
-      <Route path="/exercise/:id" element={<ExerciseDetails/>} />
-      </Routes>
-      <Footer/>
-  </Box>;
+  return (
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+      <Navbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<ExerciseDetails />} />
+        </Routes>
+      </Suspense>
+      <Footer />
+    </Box>
+  );
 }
 
 export default App;
